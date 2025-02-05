03c7e72049e8efecf13fbc089d97685373cf02d7
Anna <zaritskayaanya@yandex.ru>
2025-02-05 15:38:21 +0300

    Change the file script.js

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+//ЗАДАНИЕ 1
 let a = 20; // Переменная а равна 20
 alert(a); //Вывод
 
+
+//ЗАДАНИЕ 2
 const date = 2007; //Год выпуска первого айфона
 alert(date); //Вывод
 
+
+//ЗАДАНИЕ 3
 let name = "Брендан Эйх"; //Имя создателя языка JavaScript
-alert(name);
+alert(name); //Вывод
+
+//ЗАДАНИЕ 4
+let sum = 10 + 2; //Сумма
+alert(sum); //Вывод
+
+let difference = 10 - 2; //Разность
+alert(difference); //Вывод
+
+let product = 10 * 2; //Произведение
+alert(product); //Вывод
+
+let quotient = 10 / 2; //Частное
+alert(quotient); //Вывод
