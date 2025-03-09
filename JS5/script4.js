05bb45cbb2cf128a79fb6b4886ce1f0e1a558c51
Ann Milovanova <zaritskayaanya@yandex.ru>
2025-03-09 08:27:36 +0300

    Исправления

--- JS5/script4.js
+++ JS5/script4.js
@@ -1,13 +1,12 @@
 function age() {
-    let age = +prompt('Сколько тебе лет?')
+    let age = +prompt('Сколько тебе лет?');
     if (age < 0) {
         alert ('Вы ввели неправильное значение');
-    } else if (age >= 0 || age <= 12)  {
+    } else if (age >= 0 || age <= 12) {
         alert ('Привет, друг!');
-    } else {
+    } else if (age >= 13) {
         alert ('Добро пожаловать!');
-    } else {
-        alert ('Вы ввели неправильное значение');
-    }
+    } else ('Вы ввели неправильное значение');
 }
-    
+
+console.log(age);
